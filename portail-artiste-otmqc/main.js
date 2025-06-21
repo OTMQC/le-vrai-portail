0340e42c53703bfe251790c80782086c5d3272e8
OTMQC <julien@onthemapqc.com>
2025-06-21 19:51:43 -0400

    dailleurs

--- portail-artiste-otmqc/main.js
+++ portail-artiste-otmqc/main.js
@@ -1,6 +1,5 @@
 import { renderHeader } from "./components/header.js";
 import { renderFooter } from "./components/footer.js";
-import { renderPlaylistManager } from "./components/sections/playlistManager.js";
 import { route } from "./router.js";
 
 document.addEventListener("DOMContentLoaded", () => {
